c6cb1394e59985f5570beb83a524b25d1a38a8e7
vinv <vi.nongvan@axonactive.com>
2023-11-15 17:21:32 +0700

    TA-62: adding process step to determine step

--- decision-component-utils-demo/src/com/axonivy/utils/decisioncomponent/demo/managedbean/ReviewTicketBean.java
+++ decision-component-utils-demo/src/com/axonivy/utils/decisioncomponent/demo/managedbean/ReviewTicketBean.java
@@ -1,10 +1,12 @@
 package com.axonivy.utils.decisioncomponent.demo.managedbean;
 
+import com.axonivy.utils.decisioncomponent.demo.entity.TicketRequest;
+
 public class ReviewTicketBean extends AbstractApprovalDecisionBean {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public ReviewTicketBean() {
+	public ReviewTicketBean(TicketRequest request) {
 		init();
 	}
 	
--- decision-component-utils-demo/src/com/axonivy/utils/decisioncomponent/demo/managedbean/TicketProcessBean.java
+++ decision-component-utils-demo/src/com/axonivy/utils/decisioncomponent/demo/managedbean/TicketProcessBean.java
@@ -1,31 +1,39 @@
 package com.axonivy.utils.decisioncomponent.demo.managedbean;
 
 import com.axonivy.utils.decisioncomponent.demo.entity.TicketRequest;
+import com.axonivy.utils.decisioncomponent.demo.enums.ProcessStep;
 
 
 public class TicketProcessBean {
 	
 	private TicketRequest request;
 	
-	private RequestTicketBean requestTicketDecisionBean;
+	//private RequestTicketBean requestTicketDecisionBean;
+	
+	
+	private AbstractApprovalDecisionBean approvalDecisionBean;
 	
 	private boolean decisionRendered;
 	private boolean commentRendered;
 	private boolean approvalHistoryRendered;
 
-	public TicketProcessBean() {
-		init();
+	public TicketProcessBean(ProcessStep processStep) {
+		init(processStep);
 	}
 	
-	private void init() {
+	private void init(ProcessStep processStep) {
 		request = new TicketRequest();
 		
 		this.decisionRendered = true;
 		this.commentRendered = true;
 		this.approvalHistoryRendered = true;
 		
+		if(processStep == ProcessStep.REQUEST_TICKET) {
+			approvalDecisionBean = new RequestTicketBean(request);
+		}else {
+			approvalDecisionBean = new ReviewTicketBean(request);
+		}
 		
-		requestTicketDecisionBean = new RequestTicketBean(request);
 	}
 	
 	
@@ -46,16 +54,26 @@ public class TicketProcessBean {
 		this.request = request;
 	}
 
-	public RequestTicketBean getRequestTicketDecisionBean() {
-		return requestTicketDecisionBean;
-	}
-
-	public void setRequestTicketDecisionBean(RequestTicketBean requestTicketDecisionBean) {
-		this.requestTicketDecisionBean = requestTicketDecisionBean;
-	}
+//	public RequestTicketBean getRequestTicketDecisionBean() {
+//		return requestTicketDecisionBean;
+//	}
+//
+//	public void setRequestTicketDecisionBean(RequestTicketBean requestTicketDecisionBean) {
+//		this.requestTicketDecisionBean = requestTicketDecisionBean;
+//	}
+	
+	
 
 	public boolean isCommentRendered() {
 		return commentRendered;
+	}
+
+	public AbstractApprovalDecisionBean getApprovalDecisionBean() {
+		return approvalDecisionBean;
+	}
+
+	public void setApprovalDecisionBean(AbstractApprovalDecisionBean approvalDecisionBean) {
+		this.approvalDecisionBean = approvalDecisionBean;
 	}
 
 	public void setCommentRendered(boolean commentRendered) {
